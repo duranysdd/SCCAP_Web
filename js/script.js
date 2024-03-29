1f5d9834c850cbe96e33bfb771b013d7b8bc55ab
duranysd <duranysd1119@gmail.com>
2024-03-28 20:50:31 -0600

    ImpFirebase

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+console.log('hello world');
+
+function validateForm() {
+    var checkbox = document.getElementById("aceptoTerminos");
+    if (!checkbox.checked) {
+        alert("Debes aceptar los términos y condiciones para registrarte.");
+        return false; // Evita que el formulario se envíe
+    }
+    return true; // Permite que el formulario se envíe si se ha marcado el checkbox
+}
+
+function validaNumericos(event) {
+    if(event.charCode >= 41 && event.charCode <= 57){
+        return true;
+    }
+    return false;
+}
