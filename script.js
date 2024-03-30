184aba92280dd267a3c42360f6c211b479f36eed
Adrian <adrian.duran.matias@gmail.com>
2024-03-29 18:08:22 -0600

    Script listo

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function validateForm() {
+    var checkbox = document.getElementById("aceptoTerminos");
+    if (!checkbox.checked) {
+        alert("Debes aceptar los términos y condiciones para registrarte.");
+        return false; // Evita que el formulario se envíe
+    }
+    return true; // Permite que el formulario se envíe si se ha marcado el checkbox
+}
+
+function validaNumericos(event) {
+    if(event.charCode >= 41 && event.charCode <= 57){
+        return true;
+    }
+    return false;
+}
