2b130046689548bb16a39ea6768a623f665d5131
duranysd <duranysd1119@gmail.com>
2024-04-03 01:30:36 -0600

    SaveDataRegister

--- js/Form.js
+++ js/Form.js
@@ -1,3 +1,15 @@
 const formcontainer = document.querySelector('#registration-form')
 
-console.log(formcontainer)
+formcontainer.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const name = formcontainer['nombreInput'].value
+    const apPat = formcontainer['apellidoPaternoInput'].value
+    const apMat = formcontainer['apellidoMaternoInput'].value
+    const edad = formcontainer['edadInput'].value
+    const telefono = formcontainer['telefonoInput'].value
+    const email = formcontainer['emailInput'].value
+    const password = formcontainer['passwordInput'].value
+
+    console.log(name, apPat, apMat, edad, telefono, email, password)
+} )
